2f00ce15a343d2a16e4a8c4ffc619401499b920f
Emerson Almeida <emersonalmeidax@gmail.com>
2019-01-13 03:33:18 -0200

    init commit

---
+++ megafono-wordpress.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin Name: Megafono
+ * Plugin URI: https://www.megafono.host
+ * Description: This plugin adds integration between Megafono platform and your wordpress blog.
+ * Author: Megafono
+ * Author URI: https://www.megafono.host
+ * Text Domain: Text-domain
+ * Domain Path: /path-domain
+ * Version: 1.0.0
+ *
+ * @package         Megafono
+ */
+
+class Megafono {
+  public function __construct() {
+    # add_filter( 'embed_defaults', array( $this, 'embed_defaults'), 10, 2 );
+  }
+
+  // public function embed_defaults($embed_size, $url) {
+  //   if ( $this->is_megafono_url($url) ) {
+  //     $embed_size['height'] = 190;
+  //   }
+
+  //   return $embed_size;
+  // }
+
+  public function is_megafono_url($url) {
+    return preg_match('/^(.*)\/e\/([\w-]+)$/', $input_line);
+  }
+}
+
+new Megafono();
---
+++ tests/test-megafono-wordpress.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Class Megafono
+ *
+ * @package Megafono
+ */
+
+/**
+ * Sample test case.
+ */
+class MegafonoTest extends WP_UnitTestCase {
+	function test_is_megafono_url() {
+    var megafono = new Megafono();
+    $this->assertTrue( megafono->is_megafono_url("megafono.host/e/test-episode") );
+    $this->assertTrue( megafono->is_megafono_url("megafono.host/podcast/test/e/test-episode") );
+    $this->assertTrue( megafono->is_megafono_url("megafono.host/podcast/test/e/123test-episode") );
+    $this->assertFalse( megafono->is_megafono_url("megafono.host/e/?test-episode") );
+    $this->assertFalse( megafono->is_megafono_url("megafono.host/?e/test-episode") );
+		$this->assertFalse( megafono->is_megafono_url("megafono.host/e/test episode") );
+	}
+}
