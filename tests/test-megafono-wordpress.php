87898e6ec2c2c6f3495c9a5968fd241805344acd
Emerson Almeida <emersonalmeidax@gmail.com>
2019-01-19 00:21:09 -0200

    fix specs

--- tests/test-megafono-wordpress.php
+++ tests/test-megafono-wordpress.php
@@ -9,13 +9,13 @@
  * Sample test case.
  */
 class MegafonoTest extends WP_UnitTestCase {
-	function test_is_megafono_url() {
-    var megafono = new Megafono();
-    $this->assertTrue( megafono->is_megafono_url("megafono.host/e/test-episode") );
-    $this->assertTrue( megafono->is_megafono_url("megafono.host/podcast/test/e/test-episode") );
-    $this->assertTrue( megafono->is_megafono_url("megafono.host/podcast/test/e/123test-episode") );
-    $this->assertFalse( megafono->is_megafono_url("megafono.host/e/?test-episode") );
-    $this->assertFalse( megafono->is_megafono_url("megafono.host/?e/test-episode") );
-		$this->assertFalse( megafono->is_megafono_url("megafono.host/e/test episode") );
-	}
+    function test_is_megafono_url() {
+        $megafono = new Megafono();
+        $this->assertTrue($megafono->is_megafono_url("megafono.host/e/test-episode") );
+        $this->assertTrue($megafono->is_megafono_url("megafono.host/podcast/test/e/test-episode") );
+        $this->assertTrue($megafono->is_megafono_url("megafono.host/podcast/test/e/123test-episode") );
+        $this->assertFalse($megafono->is_megafono_url("megafono.host/e/?test-episode") );
+        $this->assertFalse($megafono->is_megafono_url("megafono.host/?e/test-episode") );
+        $this->assertFalse($megafono->is_megafono_url("megafono.host/e/test episode") );
+    }
 }
